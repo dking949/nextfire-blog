42296c3ca42a6b8778658bbbc239c92e314a0ca5
Darren <darren@driveyello.com>
2022-04-23 16:05:46 +1000

    Add Username creation in Firestore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,18 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import { Toaster } from 'react-hot-toast';
 import { UserContext } from '../lib/context';
+import { useUserData } from '@lib/hooks';
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
+
   return (
-    <UserContext.Provider value={{ user: {}, username: 'todo'}}>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
     </UserContext.Provider>
   );
 }
-  
-  
 
 export default MyApp
