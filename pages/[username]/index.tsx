4e0479b2a14e2cbfd8ff27df258afd1d90004797
Darren <darren@driveyello.com>
2022-04-25 16:55:31 +1000

    Add user profile feed w/ SSR

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from 'react';
+import UserProfile from '@components/UserProfile';
+import PostFeed from '@components/PostFeed';
+import { getUserWithUsername, postToJSON, firestore } from '@lib/firebase';
+import { query, collection, where, getDocs, limit, orderBy, getFirestore } from 'firebase/firestore';
 
-export default function index() {
+export async function getServerSideProps({ query: urlQuery }) {
+  const { username } = urlQuery;
+  const userDoc = await getUserWithUsername(username);
+
+  // If no user, short circuit to 404 page
+  if (!userDoc) {
+    return {
+      notFound: true,
+    };
+  }
+
+  let user, posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = query(
+      collection(getFirestore(), userDoc.ref.path, 'posts'),
+      where('published', '==', true),
+      orderBy('createdAt', 'desc'),
+      limit(5)
+    );
+    posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts },
+  }
+}
+
+export default function index({ user, posts }) {
   return (
-    <div>index</div>
+    <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} admin={false}/>
+    </main>
   )
 }
