410a31603862c1b497192cbd57794c0515badbce
Darren <darren@driveyello.com>
2022-06-02 20:29:30 +1000

    SSR + ISR Setup

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,4 +1,51 @@
-import React from 'react'
+import styles from '@styles/Post.module.css';
+import { UserContext } from '@lib/context';
+import { firestore, getUserWithUsername, postToJSON } from '@lib/firebase';
+import { doc, getDocs, getDoc, collectionGroup, query, limit, getFirestore } from 'firebase/firestore';
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    // const postRef = userDoc.ref.collection('posts').doc(slug);
+    const postRef = doc(getFirestore(), userDoc.ref.path, 'posts', slug);
+
+    // post = postToJSON(await postRef.get());
+    post = postToJSON(await getDoc(postRef) );
+
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 10000,
+  };
+}
+
+export async function getStaticPaths() {
+  // Improve my using Admin SDK to select empty docs
+  const q = query(
+    collectionGroup(getFirestore(), 'posts'),
+    limit(20)
+  )
+  const snapshot = await getDocs(q);
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return { 
+    paths,
+    fallback: 'blocking',
+  };
+}
 
 export default function PostPage() {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,77 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Loader from '@components/Loader';
+import PostFeed from '@components/PostFeed';
+import { firestore, postToJSON } from '@lib/firebase';
+import { Timestamp, query, where, orderBy, limit, collectionGroup, getDocs, startAfter, getFirestore } from 'firebase/firestore';
+import { useState } from 'react';
 
-export default function Home() {
-  return (
-    <div>
-      <span>TODO</span>
-    </div>
+// Max posts to query per page
+const LIMIT = 10;
+
+// Will run on the server anytime this page is requested
+export async function getServerSideProps(context) {
+  const ref = collectionGroup(getFirestore(), 'posts');
+  const postsQuery = query(
+    ref,
+    where('published', '==', true),
+    orderBy('createdAt', 'desc'),
+    limit(LIMIT),
   )
+
+  const posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+ 
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+
+  // Get next page in pagination query
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? Timestamp.fromMillis(last.createdAt) : last.createdAt;
+
+    const ref = collectionGroup(getFirestore(), 'posts');
+      const postsQuery = query(
+        ref,
+        where('published', '==', true),
+        orderBy('createdAt', 'desc'),
+        startAfter(cursor),
+        limit(LIMIT),
+      )
+
+    const newPosts = (await getDocs(postsQuery)).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+  return (
+    <main>
+
+      <div className="card card-info">
+        <h2>💡 Next.js + Firebase - The Full Course</h2>
+        <p>Welcome! This app is built with Next.js and Firebase and is loosely inspired by Dev.to.</p>
+        <p>Sign up for an 👨‍🎤 account, ✍️ write posts, then 💞 heart content created by other users. All public content is server-rendered and search-engine optimized.</p>
+      </div>
+     
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && posts.length > 0 && <button onClick={getMorePosts}>Load more</button>}
+
+      <Loader show={loading} />
+
+      {postsEnd && 'You have reached the end!'}
+    </main>
+  );
 }
